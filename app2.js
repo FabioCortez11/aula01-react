2d39e27aed0cad712b0fce7c86ced72cdf8bc691
FabioCortez11 <89554473+FabioCortez11@users.noreply.github.com>
2024-03-19 18:57:59 -0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,28 +1,65 @@
 import React, { useState, useRef } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View} from 'react-native';
-export default function App() {
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+export default function App() {
   const [base, setBase] = useState('');
   const [altura, setAltura] = useState('');
   const [area, setArea] = useState('');
-
+  const[mensagemErro, setMensagemErro] = useState('');
   const baseInputRef = useRef();
-}
+
 
 function CalcularArea() {
   if (base > 0 && altura > 0) {
     const areaCalculada = (parseFloat(base) * parseFloat(altura)) / 2;
-    setArea(areaCalculada.toString());
+    setArea(areaCalculada.toFixed(2).toString());
     setAltura('');
     setBase('');
     baseInputRef.current.clear();
     baseInputRef.current.focus();
   } else {
-    if(base == '' || altura == '') {
-      alert("Insira todos os dados!");
-    } else {
-      setArea(''); 
-    }
+   setMensagemErro('Insira todos os dados!')
   }
 }
+
+return (
+  <View style={styles.container}>
+    <Text>Insira os dados abaixo para o cálculo da área do triângulo</Text>
+    <TextInput
+      placeholder="Base"
+      style={styles.input}
+      keyboardType={'numeric'}
+      value={base}
+      onChangeText={(base) => setBase(base)}
+      ref={baseInputRef}/>
+      <TextInput
+        placeholder="Altura"
+        style={styles.input}
+        keyboardType={'numeric'}
+        value={altura}
+        onChangeText={(altura) => setAltura(altura)}/>
+        {mensagemErro ? <Text style= {styles.TextoErro}>{mensagemErro}</Text> : null}
+      <Button title="Calcular" onPress={CalcularArea} />
+      <Text>{area ? `Resultado: ${area}` : ''}</Text>
+      <StatusBar style ="auto" />
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    textAlign: 'center',
+  },
+  TextoErro: {
+    color: 'red',
+    marginTop: 10,
+  },
+});
